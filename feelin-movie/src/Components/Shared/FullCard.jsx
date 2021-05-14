0ec260c8e963f493f7af5ca416f66fda06dceab0
Cyd Villavicencio <Villacv@Cyds-MBP.fios-router.home>
2021-05-14 08:57:38 -0400

    actual final commit

--- feelin-movie/src/Components/Shared/FullCard.jsx
+++ feelin-movie/src/Components/Shared/FullCard.jsx
@@ -13,7 +13,6 @@ import CardHeader from "@material-ui/core/CardHeader";
 import { red } from "@material-ui/core/colors";
 import Avatar from "@material-ui/core/Avatar";
 import IconButton from "@material-ui/core/IconButton";
-import MoreVertIcon from "@material-ui/icons/MoreVert";
 import Grid from "@material-ui/core/Grid";
 import "./FullCard.css";
 import theme from "./theme";
@@ -45,7 +44,7 @@ const useStyles = makeStyles({
   },
   headline: {
     fontSize: 40,
-    padding: 40
+    padding: 40,
   },
 });
 
@@ -71,68 +70,63 @@ const FullCard = ({ toggle, setToggle }) => {
   const movie = movies.find((movie) => {
     return movie.id === currentURL.id;
   });
-  return ( 
+  return (
     <div className="background space">
-        <Grid container direction="row" justify="center" alignItems="center">
+      <Grid container direction="row" justify="center" alignItems="center">
         <Grid item>
-        <Typography
-                    className={classes.headline}
-                    color="textSecondary"
-                    gutterBottom
-          > Latest Review
-            </Typography>
-            {movie && (
-              <Card className={classes.root} variant="outlined">
-                <CardHeader
-                  avatar={
-                    <Avatar aria-label="recipe" className={classes.avatar}>
-                      {movie.fields.title[0]}
-                    </Avatar>
-                  }
-                  action={
-                    <IconButton aria-label="settings">
-                      <MoreVertIcon />
-                    </IconButton>
-                  }
-                  title={movie.fields.title}
-                  subheader={movie.fields.year}
-                />
-                <CardMedia
-                  className={classes.media}
-                  image={movie.fields.imageURL}
-                  title="Movie Image"
-                />
-                <CardContent>
-                  <Typography
-                    className={classes.title}
-                    color="primary"
-                    gutterBottom
-                  >
-                    Review
-                  </Typography>
-                  <Typography
-                    className={classes.title}
-                    color="textSecondary"
-                    gutterBottom
-                  >
-                    {movie.fields.review}
-                  </Typography>
-                  <CardActions>
-                    <DeleteButton
-                      page="FullCard"
-                      setToggle={setToggle}
-                      id={movie.id}
-                    />
-                  </CardActions>
-                </CardContent>
-              </Card>
-            )}
-          </Grid>
+          <Typography
+            className={classes.headline}
+            color="textSecondary"
+            gutterBottom
+          >
+            Latest Review
+          </Typography>
+          {movie && (
+            <Card className={classes.root} variant="outlined">
+              <CardHeader
+                avatar={
+                  <Avatar aria-label="movie" className={classes.avatar}>
+                    {movie.fields.title[0]}
+                  </Avatar>
+                }
+                action={<IconButton aria-label="settings"></IconButton>}
+                title={movie.fields.title}
+                subheader={movie.fields.year}
+              />
+              <CardMedia
+                className={classes.media}
+                image={movie.fields.imageURL}
+                title="Movie Image"
+              />
+              <CardContent>
+                <Typography
+                  className={classes.title}
+                  color="primary"
+                  gutterBottom
+                >
+                  Review
+                </Typography>
+                <Typography
+                  className={classes.title}
+                  color="textSecondary"
+                  gutterBottom
+                >
+                  {movie.fields.review}
+                </Typography>
+                <CardActions>
+                  <DeleteButton
+                    page="FullCard"
+                    setToggle={setToggle}
+                    id={movie.id}
+                  />
+                </CardActions>
+              </CardContent>
+            </Card>
+          )}
         </Grid>
-      </div>
+      </Grid>
+    </div>
   );
 };
 
 export default FullCard;
-
-
